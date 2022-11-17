c4fba645abaaa3f6ddd2db7f7bd79dbbee4c2187
Erfan Ehsany <e.ehsani@ipaam.ir>
2022-11-17 15:08:11 +0330

    app initial

---
+++ erfan_blog/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from erfan_blog.users.models import User
+from erfan_blog.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
